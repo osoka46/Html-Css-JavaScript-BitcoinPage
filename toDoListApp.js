8fa4bfb02840a1e3da81d343da735e374e7f5857
osoka46 <osman.bozdag@yahoo.com>
2023-10-15 17:27:49 +0200

    .

--- toDoListApp.js
+++ toDoListApp.js
@@ -1,40 +1,43 @@
 const form=document.getElementById("task-form");
 const taskList=document.getElementById("task-list");
+const reset=document.getElementById("reset-input");
 
 
-
-form.addEventListener("submit",(event)=>{
-    event.preventDefault();
-
-    //task values 
+form.addEventListener('submit',(event)=>{
+    
     const date=document.getElementById("task-date");
     const time=document.getElementById("task-time");
-    const details=document.getElementById("task-details");
+    const details=document.getElementById("text-area");
 
+    
 
-
-    const dateValue=date.value;
-    const timeValue=time.value;
-    const detailsValue=details.value;
+    const taskContent=document.createElement("span");
+    taskContent.innerHTML=`Date: ${date.value} <br> Time: ${time.value} <br> Details: ${details.value}`;
 
     const task=document.createElement("div");
     task.className="task";
+    
+    task.appendChild(taskContent);
+    
+    //add delete btn
 
-    const taskContent=document.createElement("span");
-    taskContent.innerHTML=`Date:${dateValue} <br>Time:${timeValue} <br>Details:${detailsValue}`;
     const deleteBtn=document.createElement("span");
     deleteBtn.className="delete";
     deleteBtn.innerHTML="Delete";
 
-    task.appendChild(taskContent);
     task.appendChild(deleteBtn);
     taskList.appendChild(task);
 
-    deleteBtn.addEventListener("click",()=>{
+    deleteBtn.addEventListener('click',()=>{
         task.remove();
     })
-
+  
     date.value="";
     time.value="";
     details.value="";
+})
+reset.addEventListener('submit',()=>{
+    const date=document.getElementById("task-date").value="";
+    const time=document.getElementById("task-time").value="";
+    const details=document.getElementById("text-area").value="";
 })
